037cedec0218bb9d95854b0f080b6659c7c091d5
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-24 09:20:33 -0600

    added database with meals info, and loading page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,17 @@
 import MealsHeader from "@/components/meals-header/meals-header";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <MealsHeader />
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
