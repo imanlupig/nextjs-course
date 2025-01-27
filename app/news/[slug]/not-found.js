146b1eca930d7cf5b3c1b9cb6803fc3f668da562
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-27 13:07:00 -0600

    added not found pages

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFoundSlug(){
+  return(
+    <div className="error">
+      <h1>Not Found!</h1>
+      <p>News not found</p>
+    </div>
+
+  )
+
+}
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFoundRoot(){
+  return(
+    <div className="error">
+      <h1>Not Found!</h1>
+      <p>Page not found</p>
+    </div>
+
+  )
+
+}
