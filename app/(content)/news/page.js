34d0b8bce3013f899d54e52c1285ecd9318687c2
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-28 14:24:41 -0600

    updated data fetch features to fetch directly from server instead of sending request to backend site

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,48 +1,20 @@
-'use client';
 
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("Failed to fetch news");
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if(!response.ok){
+    throw new Error('Failed to fetch news');
   }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+  const news = await response.json();
 
   let newsContent;
 
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
-
   return (
     <>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
