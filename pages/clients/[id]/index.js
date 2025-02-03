9a89e8d6283feda51f27023ac643130a9e8428f7
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-03 09:52:07 -0600

    use router object to programatically reroute pages

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,20 @@ export default function ClientsProjectsPage(){
   const router = useRouter();
   console.log(router.query)
 
+  function loadProjectHandler(){
+    // router.push('/clients/ian/projectA');
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {
+        id: 'ian', clientprojectid: 'projecta'
+      }
+    })
+  }
+
   return (
     <div>
       <h1>The Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
