31c908e351e49414cfe5d5a81adffe66f87112e8
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-03 13:08:33 -0600

    updated to use eventlist component to events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummy-data";
+
 export default function EventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>Events</h1>
+      <h1> All Events</h1>
+      <EventList items={events} />
     </div>
   );
 }
