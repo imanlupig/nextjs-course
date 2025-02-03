03403cec12f1cc521138ff84ea0b8ce0d374a7be
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-03 11:39:14 -0600

    created button component and added icons

---
+++ components/ui/button.js
@@ -0,0 +1,6 @@
+import Link from "next/link";
+import classes from './button.module.css'
+
+export default function Button(props) {
+  return <Link href={props.link} className={classes.btn}>{props.children}</Link>;
+}
