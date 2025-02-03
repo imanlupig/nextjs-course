91f877e01c0a2fab856e29778c2688a16bb5fe81
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-03 09:44:48 -0600

    update link to use alternative href pathing using objects

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,21 +1,34 @@
 import Link from "next/link";
 
-export default function ClientsPage(){
+export default function ClientsPage() {
   const clients = [
-    {id: 'ian', name: 'ian m'},
-    { id: 'nai', name: 'm nai'}
+    { id: "ian", name: "ian m" },
+    { id: "nai", name: "m nai" },
   ];
 
   return (
     <div>
       <h1>The Clients Page</h1>
       <ul>
-        <li><Link href='/clients/ian'>Ian</Link></li>
+        <li>
+          <Link href="/clients/ian">Ian</Link>
+        </li>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}> {client.name} </Link>
-          </li>))}
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: {
+                  id: client.id,
+                },
+              }}
+            >
+              {" "}
+              {client.name}{" "}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
