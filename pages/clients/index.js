bdf03a8735b67ff0378e1d8f99a484422597b4b8
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-03 09:41:21 -0600

    use dynamic routing

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function ClientsPage(){
+  const clients = [
+    {id: 'ian', name: 'ian m'},
+    { id: 'nai', name: 'm nai'}
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        <li><Link href='/clients/ian'>Ian</Link></li>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}> {client.name} </Link>
+          </li>))}
+      </ul>
     </div>
   )
 }
