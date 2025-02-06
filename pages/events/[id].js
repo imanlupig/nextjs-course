0841be420773ebcfb4d759fc8b00dff387e94f5c
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-06 14:17:55 -0600

    added added error alert and result title.

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,6 +1,7 @@
 import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
+import ErrorAlert from "@/components/ui/error-alert";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
@@ -11,14 +12,22 @@ export default function EventDetailPage() {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event Found</p>;
+    return (
+      <ErrorAlert>
+        <p>No Event Found</p>
+      </ErrorAlert>
+    )
   }
 
   return (
     <Fragment>
-      <EventSummary title={event.title}/>
-      <EventLogistics date={event.date} address={event.location}
-      image={event.image} imageAlt={event.title}/>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
