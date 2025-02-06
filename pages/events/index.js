98bd8d2a665ec015a236d26526c928b15f5765cd
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-02-06 13:28:19 -0600

    added logic to programatically redirect to filtered events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
 import EventList from "@/components/events/event-list";
 import EventSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 
 export default function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month){
+    const fullPath = `/events/${year}/${month}`
+
+    router.push(fullPath);
+
+  }
 
   return (
     <div>
-      <EventSearch/>
+      <EventSearch onSearch={findEventsHandler}/>
       <EventList items={events} />
     </div>
   );
